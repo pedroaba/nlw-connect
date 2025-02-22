99d8438dd24ed28b1cb13a7e14ef73749a42df1f
pedroaba <pedr.augustobarbosa.aparecido@gmail.com>
2025-02-22 00:48:14 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,76 @@
+import { ArrowRight, Radio, User } from 'lucide-react'
+import Image from 'next/image'
+
+import DevStageLogo from '@/assets/logo.svg'
+import { Button } from '@/components/button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+
+export default function Home() {
+  return (
+    <div className="min-h-dvh flex justify-center gap-16 flex-col">
+      <div className="flex flex-col gap-8 items-center md:items-start">
+        <Image
+          src={DevStageLogo}
+          alt="DevStage Logo"
+          width={108.5}
+          height={30}
+        />
+
+        <h1 className="text-4xl font-heading text-center leading-none font-medium flex flex-col md:text-7xl md:text-left">
+          <span className="text-blue">CodeCraft</span> Submit 2025
+        </h1>
+      </div>
+
+      <div className="flex gap-5 items-stretch flex-col md:flex-row">
+        <div className="flex-1 bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6">
+          <div className="flex items-center justify-between">
+            <h2 className="font-heading font-semibold text-gray-200 text-xl">
+              Sobre o evento
+            </h2>
+            <span className="text-purple font-semibold text-xs flex items-center gap-2">
+              <Radio className="size-5" />
+              AO VIVO
+            </span>
+          </div>
+          <p className="text-gray-300 leading-relaxed text-sm md:text-base">
+            Um evento feito por e para pessoas desenvolvedoras apaixonadas por
+            criar soluções inovadoras e compartilhar conhecimento. Vamos
+            mergulhar nas tendências mais recentes em desenvolvimento de
+            software, arquitetura de sistemas e tecnologias emergentes, com
+            palestras, workshops e hackathons.
+            <br /> <br /> Dias 15 a 17 de março | Das 18h às 21h | Online &
+            Gratuito{' '}
+          </p>
+        </div>
+        <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
+          <h2 className="font-heading font-semibold text-gray-200 text-xl">
+            Inscrição
+          </h2>
+
+          <div className="space-y-3">
+            <InputRoot>
+              <InputIcon>
+                <User />
+              </InputIcon>
+              <InputField type="text" placeholder="Nome completo" />
+            </InputRoot>
+          </div>
+
+          <div className="space-y-3">
+            <InputRoot>
+              <InputIcon>
+                <User />
+              </InputIcon>
+              <InputField type="email" placeholder="E-mail" />
+            </InputRoot>
+          </div>
+
+          <Button type="submit">
+            Confirmar
+            <ArrowRight />
+          </Button>
+        </form>
+      </div>
+    </div>
+  )
+}
